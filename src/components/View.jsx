39e56da800d62bb68e3b142b7c1f278197fa02b7
Rakesh Shrestha <dev.rakeshshrestha@gmail.com>
2023-05-11 11:58:39 +0545

    Fixes

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import GoogleSignin from "./GoogleSignin/GoogleSign";
 import AppleSingin from "./AppleSignin/AppleSingin";
-import { detectDevices } from "../helper/generalHelper";
+// import { detectDevices } from "../helper/generalHelper";
 
 export const View = () => {
   const userData = window.localStorage.getItem("user");
   const [user, setUser] = useState();
-  const OS = detectDevices();
+  // const OS = detectDevices();
 
   useEffect(() => {
     if (userData) {
@@ -24,31 +24,36 @@ export const View = () => {
   return (
     <div className="containter mt-5">
       {user ? (
-        <div className="card mt-4">
-          <div className="card-body">
-            <img src={user?.picture} alt="" />
-            <p className="mt-3">
-              <div className="row">
-                <div className="col d-flex justify-content-start">
-                  <b>Name</b>
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="card mt-4">
+            <div className="card-body">
+              <img src={user?.picture} alt="" />
+              <p className="mt-3">
+                <div className="row">
+                  <div className="col d-flex justify-content-start">
+                    <b>Name</b>
+                  </div>
+                  <div className="col d-flex justify-content-end">
+                    {user?.name}
+                  </div>
                 </div>
-                <div className="col d-flex justify-content-end">
-                  {user?.name}
-                </div>
-              </div>
 
-              <div className="row">
-                <div className="col d-flex justify-content-start">
-                  <b>Email</b>
+                <div className="row">
+                  <div className="col d-flex justify-content-start">
+                    <b>Email</b>
+                  </div>
+                  <div className="col d-flex justify-content-end">
+                    {user?.email}
+                  </div>
                 </div>
-                <div className="col d-flex justify-content-end">
-                  {user?.email}
-                </div>
-              </div>
-            </p>
-            <button className="btn btn-sm btn-danger" onClick={() => logout()}>
-              Logout
-            </button>
+              </p>
+              <button
+                className="btn btn-sm btn-danger"
+                onClick={() => logout()}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
       ) : (
@@ -56,11 +61,11 @@ export const View = () => {
           <div className="card">
             <div className="card-body p-4">
               <h5 className="text-center mb-3">Please login to continue</h5>
-              {OS?.osName === "MacOS" && (
-                <div className="mb-3">
-                  <AppleSingin />
-                </div>
-              )}
+              {/* {OS?.osName === "MacOS" && ( */}
+              <div className="mb-3">
+                <AppleSingin />
+              </div>
+              {/* // )} */}
               <GoogleSignin />
             </div>
           </div>
